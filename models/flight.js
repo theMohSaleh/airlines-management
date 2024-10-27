4dc227fbf18fa651b69a8ae93ba81aab51cd6637
Mohamed Saleh <mohxbox36021@gmail.com>
2024-10-27 14:48:50 +0300

    added flight model

---
+++ models/flight.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const flightSchema = new mongoose.Schema({
+    departure: {
+        type: String,
+        required: true,
+    },
+    destination: {
+        type: String,
+        required: true,
+    },
+    capacity: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    bookedByUsers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+        }
+    ],
+});
+
+const Flight = mongoose.model("Flight", flightSchema);
+
+module.exports = Flight;
