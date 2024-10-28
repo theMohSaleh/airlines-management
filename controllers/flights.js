c6ed354e8d7b092558d11f495cdd880e95fba4f3
Mohamed Saleh <mohxbox36021@gmail.com>
2024-10-28 11:22:33 +0300

    edit page, date needs fixing

--- controllers/flights.js
+++ controllers/flights.js
@@ -60,7 +60,12 @@ router.delete('/:flightId', async (req, res) => {
 router.get('/:flightId/edit', async (req, res) => {
     const flightId = req.params.flightId;
     const flight = await Flight.findById(flightId);
-
+    // get date as string
+    const dateString = flight.date.toISOString();
+    // remove the milliseconds and timezone from the date so it can be assigned in the input field as a value
+    const formattedDate = dateString.substring(0, dateString.indexOf('.'));
+    // assign formattedDate to
+    flight.formattedDate = formattedDate;
     res.render('flights/edit.ejs', { flight });
 })
 
